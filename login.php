9b00b0bfa56ce2f43bdba73e61649f1975993386
Bibek98049 <87264489+Bibek98049@users.noreply.github.com>
2021-07-31 23:21:15 +1000

    Add files via upload

---
+++ login.php
@@ -0,0 +1,40 @@
+<!Doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="style.css">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<title> Astra app </title>
+</head>
+
+<body>
+<div class="loginbox">
+<h1> Login Here </h1>
+<div class="col-md-3 col-lg-3 col-xs-3 col-sm-3">
+</div>
+<div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
+
+<form>
+<label id="uname" <b>Username</b></label>
+<input type="text" placeholder="Enter Username" name="uname" required >
+<br> <label id="psw" <b>Password</b></label>
+<input type="text" placeholder="Enter Password" name="psw" required >
+<input type="submit" name="sub" value="Login"> <br>
+<a href="#"> Forgot password ? </a> <br>
+<a href="#"> Sign Up </a>
+</form>
+</div>
+<div class="col-md-3 col-lg-3 col-xs-3 col-sm-3">
+</div>
+</div>
+
+</body>
+</html>  
